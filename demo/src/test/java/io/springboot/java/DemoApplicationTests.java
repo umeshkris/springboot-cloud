fb1eeca51d60a7145efed2372e11b6774af99d92
umeshkris <umesh.kris@gmail.com>
2023-04-26 22:58:31 +0530

    inital-commit

---
+++ demo/src/test/java/io/springboot/java/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springboot.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
